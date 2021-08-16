5d5758e43913d3c6db2e487ff33637f318252148
hunata <hunata87@gmail.com>
2021-08-16 11:25:36 +0700

    Thuc hanh chuoi 1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,15 +1,49 @@
 import java.util.Scanner;
 
-    public class App {
-        public static void main(String[] args){
-            Scanner sc = new Scanner(System.in);
-            System.out.print("Nhập vào chuỗi ");
-            String s = sc.nextLine();
-            System.out.println(s);
-            for (int i =0; i<s.length(); i++)
-                if (s.charAt(i)>='A'&& s.charAt(i)<'Z'){
-                    System.out.println(s.charAt(i));
-                }
-        }
+public class App {
+    public static void main(String[] args) {
+        String s = inputString();
+        printUppercase(s);
+        printLetter(s);
+        char c = inputChar();
+        int count = countChar(s, c);
+        if (count > 0) {
+            System.out.format("Có %d ký tự %s trong chuỗi", count, c);
+        } else System.out.format("Không có ký tự %s nào trong chuỗi", c);
     }
+
+    public static String inputString() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập vào chuỗi: ");
+        String s = sc.nextLine();
+        return s;
+    }
+    public static char inputChar() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập vào ký tự cần kiểm ra trong chuỗi: ");
+        char c = sc.next().charAt(0);
+        return c;
+    }
+
+    public static void printUppercase(String s) {
+        for (int i = 0; i < s.length(); i++)
+            if (Character.isUpperCase(s.charAt(i)))
+                System.out.println(s.charAt(i));
+    }
+
+    public static void printLetter(String s) {
+        for (int i = 0; i < s.length(); i++)
+            if (Character.isLetter(s.charAt(i)))
+                System.out.println(s.charAt(i));
+    }
+
+    public static int countChar(String s, char c) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++)
+            if (s.charAt(i) == c) {
+                count++;
+            }
+        return count;
+    }
+}
 
